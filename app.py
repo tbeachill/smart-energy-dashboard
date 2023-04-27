5d6eeb8b390d92e214b94a7bff029f5bead17641
tbeachill <82418020+tbeachill@users.noreply.github.com>
2023-04-27 01:43:27 +0100

    added standing charge card

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from dash import Dash, html, dash_table
+from dash import Dash, html, dash_table, dcc
 import os
 from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 import pandas as pd
+import plotly.express as px
+from datetime import date
+from dateutil.relativedelta import relativedelta
+import dash_bootstrap_components as dbc
 
 # set up sql connection
 load_dotenv('.env')
@@ -23,10 +27,36 @@ def sql_query(query):
 # Initialize the app
 app = Dash(__name__)
 
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
+# date 6 months in the past
+date_6m = date.today() - relativedelta(months=6)
+
+# standing charge card
+card = dbc.Card(
+    dbc.CardBody(
+        [
+            html.H3("Standing Charge", className="card-title"),
+            html.H2(sql_query("SELECT cost FROM StandingCharges WHERE tariff = 'A' AND region_code = 'M'")['cost'].to_string() + "p", className="card-subtitle"),
+        ]
+    ),
+    style={"width": "10rem",
+           'textAlign': 'center',
+            'color': colors['text']},
+)
+
 # App layout
 app.layout = html.Div([
-    html.Div(children='My First App with Data'),
-    dash_table.DataTable(data=sql_query("SELECT * FROM StandingCharges").to_dict('records'), page_size=10)
+    html.Div(children='Smart Energy Dashboard', style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }),
+    card,
+    dash_table.DataTable(data=sql_query("SELECT * FROM StandingCharges").to_dict('records'), page_size=10),
+    dcc.Graph(figure=px.histogram(sql_query("SELECT * FROM ElectricityImport WHERE tariff = 'A' AND region_code = 'M' AND date > '" + date_6m.strftime("%Y-%m-%d") + "'").to_dict('records'), x='date', y='unit_rate', histfunc='avg'))
 ])
 
 # Run the app
